c417dfcd1b5868cf7524eb4dd285906da63ab6c9
Rajiv Venugopal <rajivgopal@yahoo.com>
2019-08-10 10:22:54 +0800

    Updates

---
+++ precache-manifest.cf240441c9f1c5f5d6d73a5637075492.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19599d434ab30bf17696d5a492393ba0",
+    "url": "/the-venue-react-app/index.html"
+  },
+  {
+    "revision": "8fd3c2d4406173753e0e",
+    "url": "/the-venue-react-app/static/css/main.d6fa7445.chunk.css"
+  },
+  {
+    "revision": "30f4e06b7e055ae42b03",
+    "url": "/the-venue-react-app/static/js/2.11c09483.chunk.js"
+  },
+  {
+    "revision": "8fd3c2d4406173753e0e",
+    "url": "/the-venue-react-app/static/js/main.5c8e2266.chunk.js"
+  },
+  {
+    "revision": "77b7ae67f961ecbaeb19",
+    "url": "/the-venue-react-app/static/js/runtime~main.eeb68bb9.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/the-venue-react-app/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/the-venue-react-app/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/the-venue-react-app/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
